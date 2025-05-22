c70d32a256258863bc0a37b55f0f4d5e1675f39c
MeishaPD <putradewanm@student.ub.ac.id>
2025-05-22 22:27:34 +0700

    fix: fix view

--- views/dashboard/students/index.php
+++ views/dashboard/students/index.php
@@ -1,4 +1,4 @@
-<h2><?php echo $title ?></h2>
+<h2><?= $title ?></h2>
 
 <div class="students-container">
     <div class="action-buttons">
@@ -21,13 +21,13 @@
             <tbody>
                 <?php foreach ($students as $student): ?>
                 <tr>
-                    <td><?php echo htmlspecialchars($student['id']) ?></td>
-                    <td><?php echo htmlspecialchars($student['name']) ?></td>
-                    <td><?php echo htmlspecialchars($student['nim']) ?></td>
-                    <td><?php echo htmlspecialchars($student['address']) ?></td>
+                    <td><?= htmlspecialchars($student->id) ?></td>
+                    <td><?= htmlspecialchars($student->name) ?></td>
+                    <td><?= htmlspecialchars($student->nim) ?></td>
+                    <td><?= htmlspecialchars($student->address) ?></td>
                     <td class="action-links">
-                        <a href="?c=dashboard&m=editStudent&id=<?php echo $student['id'] ?>">Edit</a> |
-                        <a href="?c=dashboard&m=deleteStudent&id=<?php echo $student['id'] ?>" onclick="return confirm('Are you sure you want to delete this student?')">Delete</a>
+                        <a href="?c=dashboard&m=editStudent&id=<?= $student->id ?>">Edit</a> |
+                        <a href="?c=dashboard&m=deleteStudent&id=<?= $student->id ?>" onclick="return confirm('Are you sure you want to delete this student?')">Delete</a>
                     </td>
                 </tr>
                 <?php endforeach; ?>
